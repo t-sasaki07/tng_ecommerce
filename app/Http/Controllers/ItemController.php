b3c58e403cf85d34d54fd0596c8d49bb095fedae
nagayot <nagayot0220@gmail.com>
2021-06-10 22:41:11 +0900

    Update ItemController.php
    php artisan make:controller ItemController

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
 
 class ItemController extends Controller
 {
@@ -13,6 +14,19 @@ class ItemController extends Controller
      */
     public function index()
     {
-        return view('item.index');
+        $items = Item::Paginate(4);
+        return view('item.index')->with('items',$items);
+    }
+
+    /**
+     * 商品詳細画面を表示
+     * @param int $id
+     * @return view
+     */
+    public function detail(Request $request, $id)
+    {
+        $item = Item::find($id);
+
+        return view('item.detail')->with('item',$item);
     }
 }
